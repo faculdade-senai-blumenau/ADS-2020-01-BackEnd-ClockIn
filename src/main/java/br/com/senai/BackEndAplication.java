7262f9d12ba238859bdf211dfc51ce3155e5e4f7
allexxandre <viana.alexandre@hotmail.com>
2020-06-02 19:57:32 -0300

    Projeto Inicial

--- src/main/java/br/com/senai/BackEndAplication.java
+++ src/main/java/br/com/senai/BackEndAplication.java
@@ -13,20 +13,4 @@ public class BackEndAplication {
     public static void main(String[] args) {
         SpringApplication.run(BackEndAplication.class, args);
     }
-    @Bean("Bean Contact")
-    CommandLineRunner init(ContactRepository repository) {
-        return args -> {
-            repository.deleteAll();
-            LongStream.range(1, 11)
-                    .mapToObj(i -> {
-                        Contact c = new Contact();
-                        c.setName("Contact " + i);
-                        c.setEmail("contact" + i + "@email.com");
-                        c.setPhone("(111) 111-1111");
-                        return c;
-                    })
-                    .map(v -> repository.save(v))
-                    .forEach(System.out::println);
-        };
-    }
 }
--- src/main/java/br/com/senai/controller/ContactController.java
+++ src/main/java/br/com/senai/controller/ContactController.java
@@ -1,82 +1,61 @@
 package br.com.senai.controller;
 
-import br.com.senai.converter.ContactConverter;
 import br.com.senai.model.Contact;
-import br.com.senai.service.ContactService;
+import br.com.senai.repository.ContactRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import br.com.senai.model.ContactDTO;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping({"/contacts"})
 public class ContactController {
 
-    //@RequiredArgsConstructor cria construtor e injeta valor na propriedade final quando a classe é instanciada;
-    private final ContactService contactService;
-    public ContactController(ContactService contactService) {
-        this.contactService = contactService;
-    }
+    private ContactRepository contactRepository;
 
-    @GetMapping
-    public ResponseEntity<List<Contact>> getDomains() {
-        List<Contact> contacts = contactService.findAll();
+    ContactController(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
+    @GetMapping //Recuperar todos os registros (GET / contatos)
+    public ResponseEntity<List<Contact>> getContacts() {
+        List<Contact> contacts = contactRepository.findAll();
         return ResponseEntity.ok(contacts);
     }
 
-    @PostMapping
-    public ResponseEntity<ContactDTO> postContact(@RequestBody ContactDTO contactDTO) {
-        Contact contact = ContactConverter.toEntity(contactDTO);
-        Contact savedContact = contactService.save(contact);
-        ContactDTO savedDTO = ContactConverter.toDTO(savedContact);
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(savedDTO);
+    @GetMapping(path = {"/{id}"}) //Recuperar um registro por ID (GET / contatos / {id})
+    public ResponseEntity<Contact> findById(@PathVariable long id){
+        return contactRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<ContactDTO> put(@PathVariable Long contactId, @RequestBody ContactDTO contactDTO) {
-        return null;
+    @PostMapping //Crie um novo registro (POST / contatos)
+    public Contact create(@RequestBody Contact contact){
+        return contactRepository.save(contact);
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<ContactDTO> delete(@PathVariable Long contactId) {
-        return null;
+
+    @PutMapping(value="/{id}") //Atualizar um registro (PUT / contatos)
+    public ResponseEntity<Contact> update(@PathVariable("id") long id,
+                                          @RequestBody Contact contact){
+        return contactRepository.findById(id)
+                .map(record -> {
+                    record.setName(contact.getName());
+                    record.setEmail(contact.getEmail());
+                    record.setPhone(contact.getPhone());
+                    Contact updated = contactRepository.save(record);
+                    return ResponseEntity.ok().body(updated);
+                }).orElse(ResponseEntity.notFound().build());
     }
-//
-//    @GetMapping(path = {"/{id}"})
-//    public static ResponseEntity<Contact> findById(@PathVariable long id){
-//        return contactRepository.findById(id)
-//                .map(record -> ResponseEntity.ok().body(record))
-//                .orElse(ResponseEntity.notFound().build());
-//    }
-//
-//    @PostMapping
-//    public Contact create(@RequestBody Contact contact){
-//        return this.save(contact);
-//    }
 
-//    @PutMapping(value="/{id}")
-//    public ResponseEntity<Contact> update(@PathVariable("id") long id, @RequestBody Contact contact){
-//        return this.findById(id)
-//                .map(record -> {
-//                    record.setName(contact.getName());
-//                    record.setEmail(contact.getEmail());
-//                    record.setPhone(contact.getPhone());
-//                    Contact updated = repository.save(record);
-//                    return ResponseEntity.ok().body(updated);
-//                }).orElse(ResponseEntity.notFound().build());
-//    }
-//
-//    @DeleteMapping(path ={"/{id}"})
-//    public ResponseEntity<?> delete(@PathVariable("id") long id) {
-//        return ContactService.findById(id)
-//                .map(record -> {
-//                    ContactService.deleteById(id);
-//                    return ResponseEntity.ok().build();
-//                }).orElse(ResponseEntity.notFound().build());
-//    }
-
+    @DeleteMapping(path ={"/{id}"}) // Remover um registro (DELETE / Contacts / {id})
+    public ResponseEntity<?> delete(@PathVariable("id") long id) {
+        return contactRepository.findById(id)
+                .map(record -> {
+                    contactRepository.deleteById(id);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
+    }
 }
