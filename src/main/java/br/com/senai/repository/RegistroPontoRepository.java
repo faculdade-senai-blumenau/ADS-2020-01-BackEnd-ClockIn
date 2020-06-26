7c75eca174cd9f0bfa0745c5c5a20deeaf5402cd
mpmsilva <marcio_marrafao@estudante.sc.senai.br>
2020-06-25 21:50:48 -0300

    [ADS88]: custom query in registro ponto

--- src/main/java/br/com/senai/repository/RegistroPontoRepository.java
+++ src/main/java/br/com/senai/repository/RegistroPontoRepository.java
@@ -1,10 +1,19 @@
 package br.com.senai.repository;
 
 import br.com.senai.model.RegistroPontoModel;
-import br.com.senai.model.UsuarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RegistroPontoRepository extends JpaRepository<RegistroPontoModel, Integer> {
+
+    @Query("select a\n" +
+            "from registro_ponto a, usuario b\n" +
+            "where a.idUsuario = b.idUsuario\n" +
+            "and a.idUsuario = :idUsuario")
+    List<RegistroPontoModel> findRegistroPontoUsuario(@Param("idUsuario") int idUsuario);
 }
