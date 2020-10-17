fe62a2ff53a42115a7f62e5fbe0eee4758a80303
mpmsilva <marcio_marrafao@estudante.sc.senai.br>
2020-10-16 21:48:29 -0300

    [

--- src/main/java/br/com/senai/repository/SetorRepository.java
+++ src/main/java/br/com/senai/repository/SetorRepository.java
@@ -2,8 +2,15 @@ package br.com.senai.repository;
 
 import br.com.senai.model.SetorModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SetorRepository extends JpaRepository<SetorModel, Integer> {
+    @Query("select a.descricaoSetor, b.nomeUsuario\n" +
+            "from setor a, usuario b\n" +
+            "where a.idUsuario = b.idUsuario")
+    List<SetorModel> getSetorResponsavel();
 }
